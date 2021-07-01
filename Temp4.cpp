29b42775e969b87c5d1f2da7cb461887b90d1bda
Shradha Verma <haze.jace2002@gmail.com>
2021-07-01 23:07:07 +0530

    Swap function template added

--- Temp4.cpp
+++ Temp4.cpp
@@ -11,10 +11,19 @@ using namespace std;
     return avg;
 } */
 
+template <class T>
+void swwap (T &a, T &b )
+{
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
 template <class T1, class T2>
 float funcAvg (T1 a, T2 b)
 {
     float avg = (a+b)/2.0;
+    
     return avg;
 }
 int main()
@@ -26,5 +35,9 @@ int main()
     cout<<"The average of these numbers is = "<<a<<endl;
     a= funcAvg(2.1,1.1);
     cout<<"The average of these numbers is = "<<a<<endl;
+
+    int x = 5, y =7;
+    swwap(x,y);
+    cout<<x<<" "<<y;
      return 0;
 }
