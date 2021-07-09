f78deca2534f8d3fc99e52dc9576c37357a59a2c
Shradha Verma <haze.jace2002@gmail.com>
2021-07-09 20:43:01 +0530

    Queue STL

--- STL/stl6.cpp
+++ STL/stl6.cpp
@@ -5,5 +5,16 @@ using namespace std;
 //QUEUE STL
 int main() 
 {
+  queue <string> sample;
+  sample.push("Shradha");
+  sample.push("Verma");
+  sample.push("Bio");
+
+  cout<<"First element: "<<sample.front()<<endl; //Shradha
+  cout<<"Size of queue before pop(): "<<sample.size()<<endl;
+  sample.pop();
+
+  cout<<"First element: "<<sample.front()<<endl; //Verma
+  cout<<"Size of stack after pop(): "<<sample.size()<<endl;
  return 0;
 }
