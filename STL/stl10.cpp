9c94a07170b941e003148c07fa27094d04e61931
Shradha Verma <haze.jace2002@gmail.com>
2021-07-15 18:43:48 +0530

    Algorithm STL

--- STL/stl10.cpp
+++ STL/stl10.cpp
@@ -15,6 +15,28 @@ int main()
   cout<<"Lower bound: "<<lower_bound(v.begin(),v.end(),5)-v.begin()<<endl; //returns an iterator
   cout<<"Upper bound: "<<upper_bound(v.begin(),v.end(),3)-v.begin()<<endl;
 
-  
+  int a= 5, b=3;
+  cout<<"Max: "<<max(a,b)<<endl;
+  cout<<"Min: "<<min(a,b)<<endl;
+  cout<<"A: "<<a<<" B: "<<b<<endl;
+  swap(a,b);
+  cout<<"A: "<<a<<" B: "<<b<<endl;
+
+  string ab = "abcef";
+  reverse(ab.begin(),ab.end());
+  cout<<"String: "<<ab<<endl;
+
+  rotate(v.begin(),v.begin()+1, v.end());
+  cout<<"After rotate(): "<<endl;
+  for (int i:v)
+  {
+    cout<<i<<" ";
+  }
+  cout<<endl;
+  sort(v.begin(), v.end());
+   for (int i:v)
+  {
+    cout<<i<<" ";
+  }
   return 0;
 }
